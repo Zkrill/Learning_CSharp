aa3d6d734c84fcb96ae9dd6855c21807a7a0289a
Zkrill <withoutdarkinside@gmail.com>
2021-07-11 14:55:13 +0300

    Test Git 1

---
+++ ConsoleApp1Core/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 5;
+            int coll = 5;
+            int[,] massive = new int[row , coll];
+            int[,] massive1 = new int[row, coll];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < coll; j++)
+                {
+                    if (i != j)
+                    {
+                        massive[i, j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < coll; j++)
+                {
+                    Console.Write(massive[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < massive1.GetLength(0); i++)
+            {
+                for (int j = massive1.GetLength(1) - 1; j >= 0; j--)
+                {
+                    if (i + j != 4)
+                    {
+                        massive1[i, j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < massive1.GetLength(0); i++)
+            {
+                for (int j = 0; j < massive1.GetLength(1); j++)
+                {
+                    Console.Write(massive1[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(massive.GetLength(0));
+            Console.WriteLine(massive.GetLength(1));
+            Console.WriteLine();
+            Console.WriteLine(massive1.GetLength(0));
+            Console.WriteLine(massive1.GetLength(1));
+        }
+    }
+}
---
+++ Test1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[4];
+
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                testArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                Console.Write($" |{testArray[i]}| ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------");
+
+            for (int i = (testArray.Length - 1); i >= 0; i--)
+            {
+                Console.Write($" |{testArray[i]}| ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------");
+
+            int sumPara = 0;
+
+            for (int i = 0; i < testArray.Length; i++)
+            {
+
+                if (testArray[i]%2 == 0)
+                {
+                    sumPara += testArray[i];
+                }
+            }
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Parna suma = {sumPara}");
+            Console.WriteLine("----------------------");
+
+            int min = testArray[0];
+
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                if(min > testArray[i])
+                {
+                    min = testArray[i];
+                }
+            }
+
+            Console.WriteLine($"Minimum = {min}");
+        }
+    }
+}
